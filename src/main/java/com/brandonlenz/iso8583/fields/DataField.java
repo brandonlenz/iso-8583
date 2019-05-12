fd14eaa09c3e7573e9613521ebb66fa5df9bd7fa
Brandon Lenz <brandon.lenz@ul.com>
2019-05-11 22:37:32 -0400

    rework DataField to be more flexible

--- src/main/java/com/brandonlenz/iso8583/fields/DataField.java
+++ src/main/java/com/brandonlenz/iso8583/fields/DataField.java
@@ -3,35 +3,51 @@ package com.brandonlenz.iso8583.fields;
 import com.brandonlenz.iso8583.definitions.fields.FieldDefinition;
 import com.brandonlenz.iso8583.definitions.names.FieldName;
 import com.brandonlenz.iso8583.structure.Format;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataField {
 
-    private FieldDefinition fieldDefinition;
+    private final FieldDefinition definition;
     private byte[] rawData;
     private List<DataField> subfields;
 
     public DataField(FieldDefinition fieldDefinition) {
-        this.fieldDefinition = fieldDefinition;
+        this.definition = fieldDefinition;
+        this.subfields = createSubfieldsFromDefinition(fieldDefinition);
     }
 
     public FieldName getFieldName() {
-        return fieldDefinition.getFieldName();
+        return definition.getFieldName();
     }
 
     public byte[] getRawData() {
         return rawData;
     }
 
+    public void setRawData(byte[] rawData) {
+        this.rawData = rawData;
+    }
+
     public Format getFormat() {
-        return fieldDefinition.getFormat();
+        return definition.getFormat();
     }
 
     public FieldDefinition getDefinition() {
-        return fieldDefinition;
+        return definition;
     }
 
     public List<DataField> getSubfields() {
         return subfields;
+    }
+
+    private List<DataField> createSubfieldsFromDefinition(FieldDefinition fieldDefinition) {
+        List<DataField> subfieldList = new ArrayList<>();
+
+        for (FieldDefinition subfieldDefinition : fieldDefinition.getSubfieldDefinitions()) {
+            subfieldList.add(new DataField(subfieldDefinition));
+        }
+
+        return subfieldList;
     }
 }
