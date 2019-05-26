c49e21a2f017569cae5e1a7a0a2359b97e6db25e
Brandon Lenz <brandonalenz@gmail.com>
2019-05-25 22:33:21 -0400

    use more efficient String.replace() instead of String.replaceAll() method

--- src/main/java/com/brandonlenz/iso8583/structure/encoding/handlers/BinaryEncodingHandler.java
+++ src/main/java/com/brandonlenz/iso8583/structure/encoding/handlers/BinaryEncodingHandler.java
@@ -6,7 +6,7 @@ public class BinaryEncodingHandler implements EncodingHandler {
 
     @Override
     public byte[] encode(String data) {
-        data = data.replaceAll(" ", "");
+        data = data.replace(" ", "");
         return DatatypeConverter.parseHexBinary(data);
     }
 
