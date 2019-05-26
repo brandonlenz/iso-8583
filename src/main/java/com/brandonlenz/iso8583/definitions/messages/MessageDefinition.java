8356c75e9475a32efb0a8db1887d2502d508a0b5
Brandon Lenz <brandon.lenz@ul.com>
2019-05-25 22:35:40 -0400

    add TODO to explore future refactor

--- src/main/java/com/brandonlenz/iso8583/definitions/messages/MessageDefinition.java
+++ src/main/java/com/brandonlenz/iso8583/definitions/messages/MessageDefinition.java
@@ -4,7 +4,7 @@ import com.brandonlenz.iso8583.definitions.fields.FieldDefinition;
 import com.brandonlenz.iso8583.definitions.names.FieldName;
 import java.util.List;
 
-public interface MessageDefinition {
+public interface MessageDefinition { //TODO: rename as ProtocolDefinition? Abstract MessageDefinition seperately?
     List<FieldDefinition> getFieldDefinitions();
     FieldDefinition getFieldDefinition(int fieldNumber);
     FieldDefinition getFieldDefinition(FieldName fieldName);
