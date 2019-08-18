79d2006313777d3b3501ee62fa770966884ba11d
Brandon Lenz <brandonalenz@gmail.com>
2019-08-18 00:04:56 -0400

    rework construction of Iso8583Messages to use the builder pattern

--- src/main/java/com/brandonlenz/iso8583/definitions/messages/MessageDefinition.java
+++ src/main/java/com/brandonlenz/iso8583/definitions/messages/MessageDefinition.java
@@ -1,11 +1,9 @@
 package com.brandonlenz.iso8583.definitions.messages;
 
 import com.brandonlenz.iso8583.definitions.fields.FieldDefinition;
-import com.brandonlenz.iso8583.definitions.fields.names.FieldName;
 import java.util.Map;
 
-public interface MessageDefinition { //TODO: rename as ProtocolDefinition? Abstract MessageDefinition seperately?
+public interface MessageDefinition {
     Map<Integer, FieldDefinition> getFieldDefinitions();
     FieldDefinition getFieldDefinition(int fieldNumber);
-    FieldDefinition getFieldDefinition(FieldName fieldName);
 }
