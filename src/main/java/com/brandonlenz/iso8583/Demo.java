0153685f3cfc540a7373f1214d5c09b7ca4ddb91
Brandon Lenz <brandonalenz@gmail.com>
2019-08-18 21:54:22 -0400

    rename Main to Demo and resolve compliation issues

--- src/main/java/com/brandonlenz/iso8583/Demo.java
+++ src/main/java/com/brandonlenz/iso8583/Demo.java
@@ -1,11 +1,12 @@
 package com.brandonlenz.iso8583;
 
-import com.brandonlenz.iso8583.building.messages.Iso8583MessageBuilder;
-import com.brandonlenz.iso8583.definitions.messages.SampleIso8583MessageDefinition;
 import com.brandonlenz.iso8583.definitions.fields.names.FieldName;
+import com.brandonlenz.iso8583.definitions.messages.Iso8583MessageDefinition;
+import com.brandonlenz.iso8583.definitions.messages.SampleIso8583MessageDefinition;
 import com.brandonlenz.iso8583.fields.Bitmap;
 import com.brandonlenz.iso8583.fields.DataField;
 import com.brandonlenz.iso8583.messages.Iso8583Message;
+import com.brandonlenz.iso8583.messages.Iso8583MessageBuilder;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,8 +17,9 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 import javax.xml.bind.DatatypeConverter;
 
-public class Main {
+public class Demo {
 
+    private static final Iso8583MessageDefinition definition = new SampleIso8583MessageDefinition();
     private static Iso8583MessageBuilder messageBuilder;
     private static Scanner scanner = new Scanner(System.in);
 
@@ -50,10 +52,10 @@ public class Main {
                         handlePrettyPrintMessage();
                         break;
                     case PRINT_RAW_DATA:
-                    System.out.println(DatatypeConverter.printHexBinary(messageBuilder.getMessage().getRawData()));
+                    System.out.println(DatatypeConverter.printHexBinary(messageBuilder.build().getRawData()));
                     break;
                     case PRINT_DATA:
-                    System.out.println(messageBuilder.getMessage().getData());
+                    System.out.println(messageBuilder.build().getData());
                     break;
                     case EXIT:
                         return; //exit this loop, and thusly the program.
@@ -76,22 +78,22 @@ public class Main {
     }
 
     private static void printHeader() {
-        System.out.println("IIIIIIIIII   SSSSSSSSSSSSSSS      OOOOOOOOO                           888888888     555555555555555555      888888888      333333333333333   ");
-        System.out.println("I::::::::I SS:::::::::::::::S   OO:::::::::OO                       88:::::::::88   5::::::::::::::::5    88:::::::::88   3:::::::::::::::33 ");
-        System.out.println("I::::::::IS:::::SSSSSS::::::S OO:::::::::::::OO                   88:::::::::::::88 5::::::::::::::::5  88:::::::::::::88 3::::::33333::::::3");
-        System.out.println("II::::::IIS:::::S     SSSSSSSO:::::::OOO:::::::O                 8::::::88888::::::85:::::555555555555 8::::::88888::::::83333333     3:::::3");
-        System.out.println("  I::::I  S:::::S            O::::::O   O::::::O                 8:::::8     8:::::85:::::5            8:::::8     8:::::8            3:::::3");
-        System.out.println("  I::::I  S:::::S            O:::::O     O:::::O                 8:::::8     8:::::85:::::5            8:::::8     8:::::8            3:::::3");
-        System.out.println("  I::::I   S::::SSSS         O:::::O     O:::::O                  8:::::88888:::::8 5:::::5555555555    8:::::88888:::::8     33333333:::::3 ");
-        System.out.println("  I::::I    SS::::::SSSSS    O:::::O     O:::::O ---------------   8:::::::::::::8  5:::::::::::::::5    8:::::::::::::8      3:::::::::::3  ");
-        System.out.println("  I::::I      SSS::::::::SS  O:::::O     O:::::O -:::::::::::::-  8:::::88888:::::8 555555555555:::::5  8:::::88888:::::8     33333333:::::3 ");
-        System.out.println("  I::::I         SSSSSS::::S O:::::O     O:::::O --------------- 8:::::8     8:::::8            5:::::58:::::8     8:::::8            3:::::3");
-        System.out.println("  I::::I              S:::::SO:::::O     O:::::O                 8:::::8     8:::::8            5:::::58:::::8     8:::::8            3:::::3");
-        System.out.println("  I::::I              S:::::SO::::::O   O::::::O                 8:::::8     8:::::85555555     5:::::58:::::8     8:::::8            3:::::3");
-        System.out.println("II::::::IISSSSSSS     S:::::SO:::::::OOO:::::::O                 8::::::88888::::::85::::::55555::::::58::::::88888::::::83333333     3:::::3");
-        System.out.println("I::::::::IS::::::SSSSSS:::::S OO:::::::::::::OO                   88:::::::::::::88  55:::::::::::::55  88:::::::::::::88 3::::::33333::::::3");
-        System.out.println("I::::::::IS:::::::::::::::SS    OO:::::::::OO                       88:::::::::88      55:::::::::55      88:::::::::88   3:::::::::::::::33 ");
-        System.out.println("IIIIIIIIII SSSSSSSSSSSSSSS        OOOOOOOOO                           888888888          555555555          888888888      333333333333333   ");
+        System.out.println("IIIIIIIIII    SSSSSSSSSSSSSSS       OOOOOOOOO                           888888888      555555555555555555       888888888       333333333333333   ");
+        System.out.println("I::::::::I  SS:::::::::::::::S    OO:::::::::OO                       88:::::::::88    5::::::::::::::::5     88:::::::::88    3:::::::::::::::33 ");
+        System.out.println("I::::::::I S:::::SSSSSS::::::S  OO:::::::::::::OO                   88:::::::::::::88  5::::::::::::::::5   88:::::::::::::88  3::::::33333::::::3");
+        System.out.println("II::::::II S:::::S     SSSSSSS O:::::::OOO:::::::O                 8::::::88888::::::8 5:::::555555555555  8::::::88888::::::8 3333333     3:::::3");
+        System.out.println("  I::::I   S:::::S             O::::::O   O::::::O                 8:::::8     8:::::8 5:::::5             8:::::8     8:::::8             3:::::3");
+        System.out.println("  I::::I   S:::::S             O:::::O     O:::::O                 8:::::8     8:::::8 5:::::5             8:::::8     8:::::8             3:::::3");
+        System.out.println("  I::::I    S::::SSSS          O:::::O     O:::::O                  8:::::88888:::::8  5:::::5555555555     8:::::88888:::::8      33333333:::::3 ");
+        System.out.println("  I::::I     SS::::::SSSSS     O:::::O     O:::::O ---------------   8:::::::::::::8   5:::::::::::::::5     8:::::::::::::8       3:::::::::::3  ");
+        System.out.println("  I::::I       SSS::::::::SS   O:::::O     O:::::O -:::::::::::::-  8:::::88888:::::8  555555555555:::::5   8:::::88888:::::8      33333333:::::3 ");
+        System.out.println("  I::::I          SSSSSS::::S  O:::::O     O:::::O --------------- 8:::::8     8:::::8             5::::: 58:::::8     8:::::8             3:::::3");
+        System.out.println("  I::::I               S:::::S O:::::O     O:::::O                 8:::::8     8:::::8             5::::: 58:::::8     8:::::8             3:::::3");
+        System.out.println("  I::::I               S:::::S O::::::O   O::::::O                 8:::::8     8:::::8 5555555     5::::: 58:::::8     8:::::8             3:::::3");
+        System.out.println("II::::::II SSSSSSS     S:::::S O:::::::OOO:::::::O                 8::::::88888::::::8 5::::::55555:::::: 58::::::88888::::::8 3333333     3:::::3");
+        System.out.println("I::::::::I S::::::SSSSSS:::::S  OO:::::::::::::OO                   88:::::::::::::88   55:::::::::::::55   88:::::::::::::88  3::::::33333::::::3");
+        System.out.println("I::::::::I S:::::::::::::::SS     OO:::::::::OO                       88:::::::::88       55:::::::::55       88:::::::::88    3:::::::::::::::33 ");
+        System.out.println("IIIIIIIIII  SSSSSSSSSSSSSSS         OOOOOOOOO                           888888888           555555555           888888888       333333333333333   ");
         System.out.println();
     }
 
@@ -105,25 +107,25 @@ public class Main {
     }
 
     private static void handleNewMessage(){
-        messageBuilder = new Iso8583MessageBuilder(new SampleIso8583MessageDefinition());
         System.out.println("A new ISO-8583 Message has been created for you.");
-        handleSetMessageTypeIndicator();
+        DataField messageTypeIndicator = handleSetMessageTypeIndicator();
+        messageBuilder = Iso8583Message.builder(definition, messageTypeIndicator);
         System.out.println("Your new message has been created");
     }
 
-    private static void handleSetMessageTypeIndicator() {
+    private static DataField handleSetMessageTypeIndicator() {
         System.out.println("Your message defines MessageTypeId as follows:");
-        System.out.println(messageBuilder.getMessageDefinition().getMessageTypeIndicatorDefinition());
+        System.out.println(definition.getMessageTypeIndicatorDefinition());
         System.out.print("Please enter the messageTypeId for your new message >");
         String mtiData = scanner.next();
-        messageBuilder.setMessageTypeIndicator(mtiData);
+        return definition.getMessageTypeIndicatorDefinition().getDataFieldBuilder().build(mtiData);
     }
 
     private static void handleAddField() {
         System.out.print("Enter the field number >");
         int fieldNumber = scanner.nextInt();
         System.out.println("Field " + fieldNumber +" is defined as follows:");
-        System.out.println(messageBuilder.getMessageDefinition().getFieldDefinition(fieldNumber));
+        System.out.println(definition.getFieldDefinition(fieldNumber));
         System.out.print("Enter the data for the field >");
         String fieldData = scanner.next();
         messageBuilder.setField(fieldNumber, fieldData);
@@ -136,7 +138,7 @@ public class Main {
     }
 
     private static void handlePrettyPrintRaw() {
-        List<DataField> allFields = getAllDataFields(messageBuilder.getMessage());
+        List<DataField> allFields = getAllDataFields(messageBuilder.build());
         int prettyPrintOffset = getPrettyPrintOffset(allFields);
 
         for (DataField dataField : allFields) {
@@ -146,7 +148,7 @@ public class Main {
     }
 
     private static void handlePrettyPrintMessage() {
-        List<DataField> allFields = getAllDataFields(messageBuilder.getMessage());
+        List<DataField> allFields = getAllDataFields(messageBuilder.build());
         int prettyPrintOffset = getPrettyPrintOffset(allFields);
 
         for (DataField dataField : allFields) {
@@ -184,34 +186,36 @@ public class Main {
                 .max()
                 .orElse(20) + 5;
     }
+
+    enum Option {
+        NEW_MESSAGE(1),
+        ADD_FIELD(2),
+        REMOVE_FIELD(3),
+        PRETTY_PRINT_RAW(4),
+        PRETTY_PRINT_MESSAGE(5),
+        PRINT_RAW_DATA(6),
+        PRINT_DATA(7),
+        EXIT(9),
+        UNKNOWN(-1);
+
+        private int intValue;
+
+        Option(int value) {
+            this.intValue = value;
+        }
+
+        public int getIntValue() {
+            return intValue;
+        }
+
+        public static Option getOptionFromInt(int intValue) {
+            return Arrays.stream(Option.values())
+                    .filter(o -> o.intValue == intValue)
+                    .findFirst()
+                    .orElse(UNKNOWN);
+        }
+    }
 }
 
-enum Option {
-    NEW_MESSAGE(1),
-    ADD_FIELD(2),
-    REMOVE_FIELD(3),
-    PRETTY_PRINT_RAW(4),
-    PRETTY_PRINT_MESSAGE(5),
-    PRINT_RAW_DATA(6),
-    PRINT_DATA(7),
-    EXIT(9),
-    UNKNOWN(-1);
 
-    private int intValue;
-
-    Option(int value) {
-        this.intValue = value;
-    }
-
-    public int getIntValue() {
-        return intValue;
-    }
-
-    public static Option getOptionFromInt(int intValue) {
-        return Arrays.stream(Option.values())
-                .filter(o -> o.intValue == intValue)
-                .findFirst()
-                .orElse(UNKNOWN);
-    }
-}
 
